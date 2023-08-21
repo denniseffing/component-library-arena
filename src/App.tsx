4c157522a06b357614f223efc22bb3182f0ac79c
Jan Georg <jan.georg@codecentric.de>
2023-08-21 08:20:18 +0200

    feat: its a compent lib, its chakra yeah :note:

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,116 @@
-import { Button } from "flowbite-react";
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardHeader,
+  Checkbox,
+  Container,
+  Flex,
+  Heading,
+  Input,
+  Link,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
-export default function App() {
+function App() {
+  const [type, setType] = useState("Login");
+
+  const activeProps = {
+    variant: "solid",
+    color: "white",
+    colorScheme: "pink",
+    bgGradient: "linear(to-r, #aa44b1, #f5439b)",
+  };
+
+  const inactiveProps = {
+    variant: "outline",
+    borderColor: "gray.300",
+    borderWidth: 1.5,
+    color: "black",
+    colorScheme: "white",
+  };
+
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <Flex
+      h="100%"
+      alignItems={"center"}
+      bgGradient={"linear(to-r, #aa44b1, #f5439b)"}
+    >
+      <Container maxW="400" color="white" mt={"auto"} mb={"auto"}>
+        <Card boxShadow={"md"} p="4">
+          <CardHeader>
+            <Heading as="h1" textAlign={"center"}>
+              {type} Form
+            </Heading>
+          </CardHeader>
+          <CardBody>
+            <VStack spacing={"5"}>
+              <ButtonGroup isAttached w={"100%"}>
+                <Button
+                  {...(type === "Login" ? activeProps : inactiveProps)}
+                  w="50%"
+                  onClick={() => setType("Login")}
+                >
+                  Login
+                </Button>
+                <Button
+                  {...(type === "Signup" ? activeProps : inactiveProps)}
+                  w="50%"
+                  onClick={() => setType("Signup")}
+                >
+                  Signup
+                </Button>
+              </ButtonGroup>
+              <Input
+                size="md"
+                placeholder="Email Address"
+                focusBorderColor="#f5439b"
+              />
+              <Input
+                size="md"
+                placeholder="Password"
+                focusBorderColor="#f5439b"
+              />
+              {type === "Signup" && (
+                <Input
+                  size="md"
+                  placeholder="Confirm Password"
+                  focusBorderColor="#f5439b"
+                />
+              )}
+              <Flex
+                direction={"column"}
+                w="100%"
+                alignContent={"start"}
+                gap={2}
+              >
+                <Link color={"#f5439b"}>Forgot password?</Link>
+                {type === "Login" && (
+                  <Checkbox colorScheme="pink"> Keep me signed in </Checkbox>
+                )}
+              </Flex>
+              <Button
+                w={"100%"}
+                bgGradient={"linear(to-r, #aa44b1, #f5439b)"}
+                color={"white"}
+                colorScheme="pink"
+              >
+                {type}
+              </Button>
+              {type === "Login" && (
+                <Text>
+                  Not a member? <Link color={"#f5439b"}>Signup now!</Link>
+                </Text>
+              )}
+            </VStack>
+          </CardBody>
+        </Card>
+      </Container>
+    </Flex>
   );
 }
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      'html, body, #root': {
+        height: "100%"
+      },
+      
+    }),
+  },
+})
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
